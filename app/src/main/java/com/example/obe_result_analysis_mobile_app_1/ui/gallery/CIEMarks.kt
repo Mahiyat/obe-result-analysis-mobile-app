44d299e9ba23d64eb015b98763548b634cbd6fd5
Mahiyat <jucse29.361@gmail.com>
2024-10-19 16:06:25 +0600

    Add Overall CIE Marksheet UI
    Add Toast to Submit Marks onclick

---
+++ app/src/main/java/com/example/obe_result_analysis_mobile_app_1/ui/gallery/CIEMarks.kt
@@ -0,0 +1,10 @@
+package com.example.obe_result_analysis_mobile_app_1.ui.gallery
+
+data class CIEMarks(
+    val roll: Int,
+    val tutorial: Int,
+    val assignment: Int,
+    val curricular: Int,
+    val quiz: Int,
+    val total: Int
+)
