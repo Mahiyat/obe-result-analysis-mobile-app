373f24d5f73cfca4f23b5d3dcb2e95ace5c00389
Mahiyat <jucse29.361@gmail.com>
2024-10-28 16:07:03 +0600

    Connect backend API to dashboard
    Render markdown for graph comment generation

---
+++ app/src/main/java/com/example/obe_result_analysis_mobile_app_1/ui/home/Activity.kt
@@ -0,0 +1,8 @@
+package com.example.obe_result_analysis_mobile_app_1.ui.home
+
+data class Activity(
+    val title: String,
+    val time: String,
+    val details: String,
+    val courseName: String
+)
